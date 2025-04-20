e7d8ef65fb92f55dcff2788f5974ee024667f5ed
Esraa-1-hash <Esraa9roka111995@gmail.com>
2025-04-20 12:09:08 +0200

    Add files via upload

---
+++ shopping.py
@@ -0,0 +1,16 @@
+x=700
+print("x=",x)
+y=1200
+print("y=",y)
+h=900
+print("h=",h)
+Total_cost=x+y+h
+print("Total cost=" , Total_cost )
+Total_budget=2500
+print("total budget=" , Total_budget )
+#the Total cost is more than the Total budget.
+Total_cost=True
+Total_cost=x+y+h
+print("Total_cost"+""+">"+""+"Total_budget")
+money_needed=Total_cost-Total_budget
+print("money needed=" , money_needed )
